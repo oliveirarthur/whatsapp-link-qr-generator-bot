0a329d71b032e9ea77c3a9602058861ab63f565b
Arthur Oliveira <arthur.oliveira@hotmail.com.br>
2024-04-17 08:10:28 -0300

    feat: Implements `x-telegram-bot-api-secret-token` header

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,15 @@ import { defineString } from "firebase-functions/params";
 import { onRequest } from "firebase-functions/v2/https";
 import { bot } from "./services/telegraf.service";
 
+const BOT_SECRET = defineString("BOT_SECRET");
+
 export const botHandler = onRequest((req, res) => {
   logger.info("Request received:", { body: req.body, headers: req.headers });
+
+  if (BOT_SECRET.value() !== req.headers["x-telegram-bot-api-secret-token"]) {
+    throw new Error("Secret token does not match");
+  }
+
   return bot.handleUpdate(req.body, res);
 });
 
@@ -17,7 +24,9 @@ export const setup = onRequest(async (req, res) => {
   const BOT_ENDPOINT = defineString("BOT_ENDPOINT").value();
 
   const results = await Promise.all([
-    bot.telegram.setWebhook(BOT_ENDPOINT),
+    bot.telegram.setWebhook(BOT_ENDPOINT, {
+      secret_token: BOT_SECRET.value(),
+    }),
     bot.telegram.setMyCommands([
       {
         command: "start",
