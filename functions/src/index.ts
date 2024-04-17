5909d7226b51f4f7972a3893d296ae357d4f0680
Arthur Oliveira <arthur.oliveira@hotmail.com.br>
2024-04-17 08:10:27 -0300

    feat: Adds setup endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,28 @@
+import * as logger from "firebase-functions/logger";
 import { onRequest } from "firebase-functions/v2/https";
 import { bot } from "./services/telegraf.service";
 
-export const helloWorld = onRequest((req, res) => {
+export const botHandler = onRequest((req, res) => {
+  logger.info("Request received:", { body: req.body, headers: req.headers });
   return bot.handleUpdate(req.body, res);
+});
+
+export const setup = onRequest(async (req, res) => {
+  logger.warn("Setup request received:", {
+    body: req.body,
+    headers: req.headers,
+  });
+
+  const results = await Promise.all([
+    bot.telegram.setWebhook(process.env.BOT_ENDPOINT!),
+    bot.telegram.setMyCommands([
+      {
+        command: "start",
+        description: "Mostra uma mensagem com instruções de como usar o bot",
+      },
+    ]),
+  ]);
+
+  res.json(results);
+  return;
 });
